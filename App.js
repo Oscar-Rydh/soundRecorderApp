f77faefcb3c645f345e7889479191acb966bab70
Oscar Rydh <oscar.rydh.93@gmail.com>
2018-05-09 14:02:43 +0900

    Recorder now working with sending emails

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { Audio, Permissions } from 'expo';
+import { Audio, Permissions, FileSystem, MailComposer } from 'expo';
 
 
 export default class App extends React.Component {
 
   async play_sound() {
-
     const { Permissions } = Expo;
     let { status } = await Permissions.getAsync(Permissions.AUDIO_RECORDING);
     console.log(status)
@@ -14,24 +13,74 @@ export default class App extends React.Component {
       status = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
     }
     if (status === 'granted') {
-      //await Audio.setIsEnabledAsync(true)
+      /*
+      Prepare audio play
+      */
       const soundObject = await new Audio.Sound();
+      await soundObject.loadAsync(require('./assets/sounds/sine3.wav'))
+
+      /*
+      Prepare audio recording
+      */
       const recorder = await new Audio.Recording();
       await Audio.setIsEnabledAsync(true)
-      try {
-        await soundObject.loadAsync(require('./assets/sounds/test.m4a'));
-        console.log("Prepering recording")
-        await recorder.prepareToRecordAsync()
-        await recorder.startAsync();
-        const result = await recordingInstance.stopAndUnloadAsync();
-        console.log(result)
-        // You are now recording!
-        await soundObject.playAsync();
-        // Your sound is playing!
-      } catch (error) {
-        // An error occurred!
-        console.log(error)
-      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        shouldDuckAndroid: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+      });
+      await recorder.prepareToRecordAsync({
+        ios: {
+          extension: '.wav',
+          outputFormat: Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_LINEARPCM,
+          audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
+          sampleRate: 44100,
+          numberOfChannels: 1,
+          bitRate: 96400,
+          linearPCMBitDepth: 16,
+          linearPCMIsBigEndian: false,
+          linearPCMIsFloat: false,
+        },
+        android: {
+          extension: '.wav',
+          outputFormat: Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_LINEARPCM,
+          audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
+          sampleRate: 44100,
+          numberOfChannels: 1,
+          bitRate: 96400,
+          linearPCMBitDepth: 16,
+          linearPCMIsBigEndian: false,
+          linearPCMIsFloat: false,
+        }
+      });
+
+      // Start audio recording after 5 seconds
+      setTimeout( () => recorder.startAsync(), 5000);
+
+      // Play the audio sweep in 500 ms
+      setTimeout(() => soundObject.playAsync(), 5100)
+
+      // Stop audio recording
+      setTimeout(async () => {
+        await recorder.stopAndUnloadAsync();
+
+        // Send the file
+        const newURI = FileSystem.cacheDirectory + 'randomNameHere.wav'
+
+        await FileSystem.moveAsync({
+          from: recorder.getURI(),
+          to: FileSystem.cacheDirectory + 'randomNameHere.wav'
+        })
+
+        MailComposer.composeAsync({
+          recipients: ["oscar.rydh.93@gmail.com", "joel.klint@gmail.com"],
+          subject: "Mobile Computing Recording",
+          attachments: [newURI]
+        })
+
+      }, 6000);
     }
 
   }
